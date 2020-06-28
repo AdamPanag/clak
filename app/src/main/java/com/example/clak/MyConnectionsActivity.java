9934a3415d703775b27dd88f47017a9d4a5aa457
AdamPanag <adampanag06@gmail.com>
2020-06-29 02:28:23 +0300

    Add MyConnections grid layout

--- app/src/main/java/com/example/clak/MyConnectionsActivity.java
+++ app/src/main/java/com/example/clak/MyConnectionsActivity.java
@@ -1,16 +1,90 @@
 package com.example.clak;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.widget.Toast.makeText;
 
 public class MyConnectionsActivity extends AppCompatActivity {
 
+    private final static String TAG = "MY CONNECTIONS";
+
+    private Toolbar toolbar;
+    private RecyclerView dataList;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_connections);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("My Connections");
+        setSupportActionBar(toolbar);
+        dataList = findViewById(R.id.dataList);
+
+        initFirebaseAuth();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.customer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_toolbar:
+                logoutUser();
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void initFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    updateUI();
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    private void updateUI() {
+
+    }
+
+    private void logoutUser() {
+        startActivity(new Intent(MyConnectionsActivity.this, LoginActivity.class));
+        FirebaseAuth.getInstance().signOut();
+        makeText(MyConnectionsActivity.this, R.string.see_you_soon, Toast.LENGTH_SHORT).show();
     }
 
     private void populateList() {
